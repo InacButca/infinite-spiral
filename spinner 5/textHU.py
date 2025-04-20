230b27179b9a85d7f22b7426479fa4418b877cbb
InacButca <calibutcun@gmail.com>
2025-04-20 18:42:24 +0200

    Add files via upload

---
+++ spinner 5/textHU.py
@@ -0,0 +1,42 @@
+caption = "Végtelen spirál"
+fps = f"Képkockák másodpercenként: "
+
+instructions = [
+    "---Irányítások---",
+    "Szóköz:                      Új négyzetet hoz létre.",
+    "Bal egérgomb:             Elhúzza a középső négyzetet.",
+    "Középső egérgomb:     Vált változók között.",
+    "Görgő fel:                    Az adott változót növeli.",
+    "Görgő le:                     Az adott változót csökkenti.",
+    "Jobb egérgomb:          Törli az előzőleg létre hozott négyzetet.",
+    "OK"
+    ]
+
+variables = [
+    "Sebesség: ",
+    " fok/képkocka",
+    "Méret: ",
+    " px^2",
+    "Sugár: ",
+    " px",
+    "Piros: ",
+    " színérték",
+    "Zöld: ",
+    " színérték",
+    "Kék: ",
+    " színérték",
+    "Semmi"
+]
+
+developer = [
+    "Készítette: Inactrill",
+    "",
+    "Github:",
+    "   https://github.com/InacButca/infinite-spiral",
+    "",
+    "Zene: C418",
+    "",
+    "",
+    "",
+    "Köszönöm hogy játszottál!",
+]
